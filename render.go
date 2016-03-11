343effabcf421db5d37720839738294a77a305ea
Ben Friedland <bfriedland@shapesecurity.com>
2016-03-11 13:27:57 -0800

    Resume parsing

--- render.go
+++ render.go
@@ -8,28 +8,70 @@ import (
 
 func main() {
     fmt.Printf("Pulling resume repository.\n")
-    r, err := git.NewRepository("https://github.com/ben174/bugben", nil)
+
+    remote, err := git.NewRemote("https://github.com/ben174/bugben")
+
     if err != nil {
         panic(err)
     }
 
+    if err := remote.Connect(); err != nil {
+        panic(err)
+    }
+
+    fmt.Println(remote.Info().Head)
+
+
+    repository, err := git.NewRepository("https://github.com/ben174/bugben", nil)
+    if err != nil {
+        panic(err)
+    }
+
+    if err := repository.Pull("origin", "refs/heads/master"); err != nil {
+        panic(err)
+    }
+
+    head := remote.Info().Head
+
+    fmt.Println("Fetching Head:", head)
+
+
+    commit, err := repository.Commit(head)
+    entries := commit.Tree().Entries
+
+
+    for i, t := range entries {
+      fmt.Println(i, t)
+    }
+
+
+
+    treewalker := git.NewTreeWalker(repository, commit.Tree())
+
+    for {
+      name, _, _, err := treewalker.Next()
+      if err == io.EOF {
+        break
+      }
+      fmt.Println(name)
+    }
+
+
+
+    /*
     if err := r.Pull("origin", "refs/heads/master"); err != nil {
         panic(err)
     }
 
     iter := r.Commits()
+
     defer iter.Close()
 
-    for {
-        commit, err := iter.Next()
-        if err != nil {
-            if err == io.EOF {
-                break
-            }
-
-            panic(err)
-        }
-
-        fmt.Println(commit)
+    commit, err := iter.Next()
+    if err != nil {
+        panic(err)
     }
+
+    fmt.Println(commit.Message)
+    */
 }
