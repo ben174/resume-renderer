238aa651ca259c1b25e7ef6e3804814d0c0f4a0b
Ben Friedland <ben174@gmail.com>
2016-03-21 17:24:29 -0700

    New json structure

--- render.go
+++ render.go
@@ -1,77 +1,157 @@
 package main
 
 import (
-    "fmt"
-    "io"
-    "gopkg.in/src-d/go-git.v3"
+	"fmt"
+	//"io"
+	"encoding/json"
+
+	"gopkg.in/src-d/go-git.v3"
 )
 
+type Response1 struct {
+	Page   int
+	Fruits []string
+}
+
+type Response2 struct {
+	Page   int      `json:"page"`
+	Fruits []string `json:"fruits"`
+}
+
+type Resume struct {
+	Skills []struct {
+		Skill string `json:"name"`
+		Years int    `json:"years"`
+	} `json:"skills"`
+
+	ProfileSummary []struct {
+		SummaryEntry string `json:"entry"`
+	} `json:"summary"`
+
+	Links []struct {
+		Title string `json:"title"`
+		URL   string `json:"url"`
+	} `json:"links"`
+
+	Links []struct {
+		Title string `json:"title"`
+		URL   string `json:"url"`
+	} `json:"links"`
+
+	Expertise []struct {
+		Category string   `json:"category"`
+		Entries  []string `json:"entries"`
+	} `json:"expertise"`
+
+	Contributions []struct {
+		Title   string   `json:"category"`
+		Entries []string `json:"entries"`
+	} `json:"expertise"`
+}
+
+type Resume struct {
+	Experience []ExperienceEntry `json:"experience"`
+}
+
+type ExperienceEntry struct {
+	Years int    `json:"years"`
+	Name  string `json:"name"`
+}
+
 func main() {
-    fmt.Printf("Pulling resume repository.\n")
+	resume := &Resume{}
+	entry := &ExperienceEntry{}
+	entry.Years = 10
+	entry.Name = "balls"
 
-    remote, err := git.NewRemote("https://github.com/ben174/bugben")
+	resume.Experience = []ExperienceEntry{entry}
 
-    if err != nil {
-        panic(err)
-    }
+	bolB, _ := json.Marshal(true)
+	fmt.Println(string(bolB))
+	intB, _ := json.Marshal(1)
+	fmt.Println(string(intB))
+	fltB, _ := json.Marshal(2.34)
+	fmt.Println(string(fltB))
+	strB, _ := json.Marshal("gopher")
+	fmt.Println(string(strB))
 
-    if err := remote.Connect(); err != nil {
-        panic(err)
-    }
+	slcD := []string{"apple", "peach", "pear"}
+	slcD = append(slcD, "balls")
+	slcB, _ := json.Marshal(slcD)
+	fmt.Println(string(slcB))
 
-    fmt.Println(remote.Info().Head)
+	mapD := map[string]int{"apple": 5, "lettuce": 7}
+	mapB, _ := json.Marshal(mapD)
+	fmt.Println(string(mapB))
+
+	res1D := &Response1{
+		Page:   1,
+		Fruits: []string{"apple", "peach", "pear"}}
+	res1B, _ := json.Marshal(res1D)
+	fmt.Println(string(res1B))
+
+	fmt.Printf("Pulling resume repository.\n")
+
+	remote, err := git.NewRemote("https://github.com/ben174/bugben")
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err := remote.Connect(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(remote.Info().Head)
+
+	repository, err := git.NewRepository("https://github.com/ben174/bugben", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := repository.Pull("origin", "refs/heads/master"); err != nil {
+		panic(err)
+	}
+
+	head := remote.Info().Head
+
+	fmt.Println("Fetching Head:", head)
+
+	commit, err := repository.Commit(head)
+	entries := commit.Tree().Entries
+
+	for i, t := range entries {
+		fmt.Println(i, t)
+	}
+
+	/*
+
+	   treewalker := git.NewTreeWalker(repository, commit.Tree())
+
+	   for {
+	     name, _, _, err := treewalker.Next()
+	     if err == io.EOF {
+	       break
+	     }
+	     fmt.Println(name)
+	   }
 
 
-    repository, err := git.NewRepository("https://github.com/ben174/bugben", nil)
-    if err != nil {
-        panic(err)
-    }
 
-    if err := repository.Pull("origin", "refs/heads/master"); err != nil {
-        panic(err)
-    }
+	   /*
+	   if err := r.Pull("origin", "refs/heads/master"); err != nil {
+	       panic(err)
+	   }
 
-    head := remote.Info().Head
+	   iter := r.Commits()
 
-    fmt.Println("Fetching Head:", head)
+	   defer iter.Close()
 
+	   commit, err := iter.Next()
+	   if err != nil {
+	       panic(err)
+	   }
 
-    commit, err := repository.Commit(head)
-    entries := commit.Tree().Entries
-
-
-    for i, t := range entries {
-      fmt.Println(i, t)
-    }
-
-
-
-    treewalker := git.NewTreeWalker(repository, commit.Tree())
-
-    for {
-      name, _, _, err := treewalker.Next()
-      if err == io.EOF {
-        break
-      }
-      fmt.Println(name)
-    }
-
-
-
-    /*
-    if err := r.Pull("origin", "refs/heads/master"); err != nil {
-        panic(err)
-    }
-
-    iter := r.Commits()
-
-    defer iter.Close()
-
-    commit, err := iter.Next()
-    if err != nil {
-        panic(err)
-    }
-
-    fmt.Println(commit.Message)
-    */
+	   fmt.Println(commit.Message)
+	*/
 }
---
+++ resume.py
@@ -0,0 +1,37 @@
+import json
+resume = {
+  'name': 'Ben Friedland',
+  'email': 'resume@bugben.com',
+  'url': 'http://www.bugben.com',
+  'profile': [
+    'Senior software/systems engineer with fifteen years of professional experience.',
+    'Unique mix of software engineering and systems administration.',
+  ],
+  'links': [
+    {'title': 'Linked-In Profile', 'url': 'http://www.linkedin.com/in/bfriedland'},
+    {'title': 'GitHub Page', 'url': 'http://github.com/ben174'},
+  ],
+  'expertise': [
+    {'category': 'Web Application Development', 'entries': ['Python', 'Django']},
+    {'category': 'Mobile Development', 'entries': ['Responsive Web Design', 'REST communication (creation and consumption)']},
+  ],
+  'community': [
+    {'title': 'HSTS Super Cookie', 'projectUrl': 'https://github.com/ben174/hsts-cookie', 'srcUrl': 'https://github.com/ben174/hsts-cookie', 'summary': 'Creates a cookie', 'body': 'Creates a super cookie'},
+    {'title': 'A&G Calendar', 'projectUrl': 'http://www.armstrongandgettybingo.com/', 'srcUrl': 'https://github.com/ben174/angrates', 'summary': 'A podcast calendar for listeners to have a resource for finding episodes of my favorite radio show.', 'body': 'Listeners of the popular radio show "The Armstrong and Getty Show" were constantly having trouble finding episodes of the podcast available online. There was no clean and easy way to find episodes. I made this site as an attempt to simplify navigation and make episodes easily searchable.'},
+  ],
+  'experience': [
+    {
+        'employer': 'KIXEYE',
+        'url': 'http://www.kixeye.com',
+        'title': 'Lead Developer',
+        'startDate': 'Creates a cookie',
+        'endDate': 'Creates a cookie',
+        'accomplishments': [
+            'Team leader in a team of seven - in an extremely fast-paced tools environment for one of the big names in video games. Used Python + Flask to develop internal tools to manage user data within several games.',
+            'Interacted with REST endpoints for several video games, creating a common UI to manage individual aspects of each game.'
+        ]
+        'logoUrl': 'http://yahoo.com'
+    },
+  ],
+}
+print json.dumps(resume)
